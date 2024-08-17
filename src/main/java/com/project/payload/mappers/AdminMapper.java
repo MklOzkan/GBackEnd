42fc90544e9df3b40403c4ceff8d19ae8ebef85e
ata324 <mavican324@gmail.com>
2024-08-17 22:03:04 +0200

    update

--- src/main/java/com/project/payload/mappers/AdminMapper.java
+++ src/main/java/com/project/payload/mappers/AdminMapper.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
-import java.util.stream.Collectors;
+
 
 @Component
 @RequiredArgsConstructor
--- src/main/java/com/project/payload/mappers/AuthenticationMapper.java
+++ src/main/java/com/project/payload/mappers/AuthenticationMapper.java
@@ -1,8 +1,6 @@
 package com.project.payload.mappers;
 
-import com.project.domain.concretes.user.User;
-import com.project.payload.request.SignUpRequest;
-import com.project.payload.response.SignInResponse;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
