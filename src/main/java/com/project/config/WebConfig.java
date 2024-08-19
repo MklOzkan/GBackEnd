9dd4710d2178e59f09423a2746afcb927ec07dbf
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-08-19 09:57:57 +0200

    End pointleri görebilmek icin swagger eklendi

--- src/main/java/com/project/config/WebConfig.java
+++ src/main/java/com/project/config/WebConfig.java
@@ -1,10 +1,18 @@
 package com.project.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@OpenAPIDefinition(info = @Info(title = "GASAN API", version = "1.0.0"),
+        security = @SecurityRequirement(name = "Bearer"))
+@SecurityScheme(name = "Bearer", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
