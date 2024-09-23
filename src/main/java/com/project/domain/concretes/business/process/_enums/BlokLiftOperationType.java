b667fa40c4f7612b0ba14b454197d955d01aad92
Mikail Ozkan <ozkan.mkl.ozkanQgmail.com>
2024-09-23 11:37:44 +0200

    new updates

--- src/main/java/com/project/domain/concretes/business/process/_enums/BlokLiftOperationType.java
+++ src/main/java/com/project/domain/concretes/business/process/_enums/BlokLiftOperationType.java
@@ -3,6 +3,7 @@ package com.project.domain.concretes.business.process._enums;
 public enum BlokLiftOperationType {
 
     BORU_KAPAMA,
+    BORU_KAYNAK,
     BLOK_LIFT_MONTAJ,
     GAZ_DOLUM,
     TEST
